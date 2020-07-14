Made changes to fix Rnet calculation; separate implementation of intermediate variable calculation in pandas

msc/calval/MOD16_setup.py
@@ -17,6 +17,16 @@ BPLUT = {'tmin_open': {'min': 8, 'max': 12, 'guess': 10},
          'rbl_min': {'min': 10, 'max': 110, 'guess': 70},
          'rbl_max': {'min': 20, 'max': 150, 'guess': 120}}
 
+BPLUT2 = [spotpy.parameter.List('tmin_open', list(np.arange(8, 13)), repeat=True),
+          spotpy.parameter.List('tmin_close', list(np.arange(-8, -5)), repeat=True),
+          spotpy.parameter.List('vpd_open', list(np.arange(0, 2000, 100)), repeat=True),
+          spotpy.parameter.List('vpd_close', list(np.arange(1450, 7000, 100)), repeat=True),
+          spotpy.parameter.List('gl_sh', list(np.arange(0, 0.12, 0.001)), repeat=True),
+          spotpy.parameter.List('gl_e_wv', list(np.arange(0, 0.12, 0.001)), repeat=True),
+          spotpy.parameter.List('Cl', list(np.arange(0, 0.013, 0.0005)), repeat=True),
+          spotpy.parameter.List('rbl_min', list(np.arange(10, 115, 5)), repeat=True),
+          spotpy.parameter.List('rbl_min', list(np.arange(20, 155, 5)), repeat=True)]
+
 
 class model(object):
 
@@ -50,17 +60,23 @@ class spotpy_setup(object):
         self.param_bounds = param_bounds
         self.use_sm = use_sm
 
+        # if self.use_sm:
+        #     self.param_bounds['sm_open'] = {'min': 0.1, 'max': 1.0, 'guess': 0.5}
+        #     self.param_bounds['sm_close'] = {'min': 0.0, 'max': 0.2, 'guess': 0.05}
+        # else:
+        #     self.param_bounds['beta'] = {'min': 0, 'max': 1000, 'guess': 250}
         if self.use_sm:
-            self.param_bounds['sm_open'] = {'min': 0.1, 'max': 1.0, 'guess': 0.5}
-            self.param_bounds['sm_close'] = {'min': 0.0, 'max': 0.2, 'guess': 0.05}
+            self.param_bounds.append(spotpy.parameter.List('sm_open', list(np.arange(0.1, 1.01, 0.01)), repeat=True))
+            self.param_bounds.append(spotpy.parameter.List('sm_close', list(np.arange(0.0, 0.21, 0.01)), repeat=True))
         else:
-            self.param_bounds['beta'] = {'min': 0, 'max': 1000, 'guess': 250}
+            self.param_bounds.append(spotpy.parameter.List('beta', list(np.arange(100, 1100, 100)), repeat=True))
 
         self.model = model(self.tmp)
-        self.params = [spotpy.parameter.Uniform(x,
-                                                low=self.param_bounds[x]['min'],
-                                                high=self.param_bounds[x]['max'],
-                                                optguess=self.param_bounds[x]['guess']) for x in self.param_bounds]
+        # self.params = [spotpy.parameter.Uniform(x,
+        #                                         low=self.param_bounds[x]['min'],
+        #                                         high=self.param_bounds[x]['max'],
+        #                                         optguess=self.param_bounds[x]['guess']) for x in self.param_bounds]
+        self.params = self.param_bounds
 
     def parameters(self):
         return spotpy.parameter.generate(self.params)
@@ -76,6 +92,7 @@ class spotpy_setup(object):
             sm_open = None
             sm_close = None
             beta = vector[9]
+        print(vector)
 
         return self.model.run(tmin_open=vector[0], tmin_close=vector[1], vpd_open=vector[2], vpd_close=vector[3],
                               gl_sh=vector[4], gl_e_wv=vector[5], Cl=vector[6], rbl_min=vector[7], rbl_max=vector[8],
@@ -135,8 +152,8 @@ if __name__ == '__main__':
                         save_name = os.path.join(parser_args.out_dir,
                                                  'nRuns-' + str(parser_args.n_runs) + '_fold-' + str(fold) +
                                                  '_group-' + grp + '_method-mcmc')
-                        sampler = spotpy.algorithms.mcmc(spotpy_setup(tmp=train_df, param_bounds=BPLUT,
-                                                                      use_sm=parser_args.use_sm),
+                        sampler = spotpy.algorithms.mcmc(spotpy_setup(tmp=train_df, param_bounds=BPLUT2,
+                                                                    use_sm=parser_args.use_sm),
                                                          dbname=save_name + '_training', dbformat='csv', save_sim=False)
                         sampler.sample(repetitions=parser_args.n_runs)
 
@@ -144,7 +161,7 @@ if __name__ == '__main__':
                         save_name = os.path.join(parser_args.out_dir,
                                                  'nRuns-' + str(parser_args.n_runs) + '_fold-' + str(fold) +
                                                  '_group-' + grp + '_method-demc')
-                        sampler = spotpy.algorithms.demcz(spotpy_setup(tmp=train_df, param_bounds=BPLUT,
+                        sampler = spotpy.algorithms.demcz(spotpy_setup(tmp=train_df, param_bounds=BPLUT2,
                                                                        use_sm=parser_args.use_sm),
                                                           dbname=save_name + '_training', dbformat='csv',
                                                           save_sim=False)

msc/models/LocalMod16_testing_changes.py
@@ -7,27 +7,27 @@ epsl = 0.622
 
 # calculate latent heat flux for wet canopy
 def LEwetfun_day(df, gl_sh, gl_e_wv):
-
-    df['rhc'] = (1.0/(gl_sh*df.LAI*df.Fwet_day))
+    df['rhc'] = (1.0 / (gl_sh * df.LAI * df.Fwet_day))
     df['t'] = df.ta_day  # + 273.15
-    df['rrc'] = df.rho_day*Cp/(4.0*SBC*df.t*df.t*df.t)
-    df['rhrc'] = (df.rhc*df.rrc)/(df.rhc+df.rrc)
-    df['rvc'] = 1.0/(gl_e_wv*df.LAI*df.Fwet_day)
-    df['LEwet_day'] = (df.s_day * df.Ac_day + df.rho_day * Cp * df.vpd_day * df.Fc * df.daylength / df.rhrc) * df.Fwet_day / (df.s_day + ((df.pa * Cp * df.rvc) / (df.lamda_day * epsl * df.rhrc)))
+    df['rrc'] = df.rho_day * Cp / (4.0 * SBC * df.t * df.t * df.t)
+    df['rhrc'] = (df.rhc * df.rrc) / (df.rhc + df.rrc)
+    df['rvc'] = 1.0 / (gl_e_wv * df.LAI * df.Fwet_day)
+    df['LEwet_day'] = (
+                              df.s_day * df.Ac_day + df.rho_day * Cp * df.vpd_day * df.Fc * df.daylength / df.rhrc) * df.Fwet_day / (
+                              df.s_day + ((df.pa * Cp * df.rvc) / (df.lamda_day * epsl * df.rhrc)))
 
     df = df.drop(columns=['t', 'rhc', 'rrc', 'rhrc', 'rvc'])
     return df
 
 
 def LEwetfun_night(df, gl_sh, gl_e_wv):
-
-    df['rhc'] = 1.0/(gl_sh*df.LAI*df.Fwet_night)
+    df['rhc'] = 1.0 / (gl_sh * df.LAI * df.Fwet_night)
     df['t'] = df.ta_night  # + 273.15
-    df['rrc'] = df.rho_night*Cp/(4.0*SBC*df.t*df.t*df.t)
-    df['rhrc'] = df.rhc*df.rrc/(df.rhc+df.rrc)
-    df['rvc'] = 1.0/(gl_e_wv*df.LAI*df.Fwet_night)
-    df['LEwet_night'] = (df.s_night * df.Ac_night + df.rho_night * Cp * df.vpd_night * df.Fc * df.nightlength / df.rhrc)\
-                         * df.Fwet_night / (df.s_night + (df.pa * Cp * df.rvc / (df.lamda_night * epsl * df.rhrc)))
+    df['rrc'] = df.rho_night * Cp / (4.0 * SBC * df.t * df.t * df.t)
+    df['rhrc'] = df.rhc * df.rrc / (df.rhc + df.rrc)
+    df['rvc'] = 1.0 / (gl_e_wv * df.LAI * df.Fwet_night)
+    df['LEwet_night'] = (df.s_night * df.Ac_night + df.rho_night * Cp * df.vpd_night * df.Fc * df.nightlength / df.rhrc) \
+                        * df.Fwet_night / (df.s_night + (df.pa * Cp * df.rvc / (df.lamda_night * epsl * df.rhrc)))
 
     df = df.drop(columns=['t', 'rhc', 'rrc', 'rhrc', 'rvc'])
     return df
@@ -36,24 +36,21 @@ def LEwetfun_night(df, gl_sh, gl_e_wv):
 # ------------------------------------------------Plant transpiration-----------------------
 # m(vpd)
 def mVPD_day(df, vpd_close, vpd_open):
-
-    df['mvpd'] = (vpd_close-df.vpd_day)/(vpd_close-vpd_open)
+    df['mvpd'] = (vpd_close - df.vpd_day) / (vpd_close - vpd_open)
     df['mvpd'] = np.clip(df.mvpd, 0, 1)
 
     return df
 
 
 def mTemp_day(df, tmin_close, tmin_open):
-
-    df['mtemp'] = (df.ta_day - tmin_close)/(tmin_open-tmin_close)
+    df['mtemp'] = (df.ta_day - tmin_close) / (tmin_open - tmin_close)
     df['mtemp'] = np.clip(df.mtemp, 0, 1)
 
     return df
 
 
 def mSM(df, sm_open, sm_close):
-
-    df['msm'] = (df['fSM-rz'] - sm_close)/(sm_open - sm_close)
+    df['msm'] = (df['fSM-rz'] - sm_close) / (sm_open - sm_close)
     df['msm'] = np.clip(df.msm, 0, 1)
 
     return df
@@ -61,10 +58,9 @@ def mSM(df, sm_open, sm_close):
 
 # daytime plant transpiration
 def LEtrans_day(df, Cl, gl_sh, vpd_open, vpd_close, tmin_open, tmin_close, sm_open, sm_close):
-
-    df['ta'] = ((df.ta_day+273.15)/293.15)**1.75
+    df['ta'] = ((df.ta_day + 273.15) / 293.15) ** 1.75
     dat = df
-    dat['rcorr'] = 1.0/((101300/dat.pa)*dat.ta)
+    dat['rcorr'] = 1.0 / ((101300 / dat.pa) * dat.ta)
 
     dat = mVPD_day(dat, vpd_close=vpd_close, vpd_open=vpd_open)
     dat = mTemp_day(dat, tmin_close=tmin_close, tmin_open=tmin_open)
@@ -74,17 +70,19 @@ def LEtrans_day(df, Cl, gl_sh, vpd_open, vpd_close, tmin_open, tmin_close, sm_op
         dat['Gs1'] = Cl * dat.mvpd * dat.mtemp * dat.msm * dat.rcorr
 
     else:
-        dat['Gs1'] = Cl*dat.mvpd*dat.mtemp*dat.rcorr
-    dat['Gcu'] = dat.rcorr*0.00001
+        dat['Gs1'] = Cl * dat.mvpd * dat.mtemp * dat.rcorr
+    dat['Gcu'] = dat.rcorr * 0.00001
     dat['Gs2'] = gl_sh
-    dat['Cc'] = dat.LAI*(1.0-dat.Fwet_day)*(dat.Gs2*(dat.Gs1+dat.Gcu))/(dat.Gs1+dat.Gs2+dat.Gcu)
+    dat['Cc'] = dat.LAI * (1.0 - dat.Fwet_day) * (dat.Gs2 * (dat.Gs1 + dat.Gcu)) / (dat.Gs1 + dat.Gs2 + dat.Gcu)
 
     dat['Cc'] = np.where((dat['LAI'] > 0) & ((1.0 - dat['Fwet_day']) > 0), dat['Cc'], 0)
-    dat['rs'] = 1.0/dat.Cc
-    dat['rh'] = 1.0/gl_sh
-    dat['rr'] = dat.rho_day*Cp/(4.0 * SBC * dat.ta_day * dat.ta_day * dat.ta_day)  # (dat.ta_day+273.15)*(dat.ta_day+273.15)*(dat.ta_day+273.15)
-    dat['ra'] = (dat.rh*dat.rr)/(dat.rh+dat.rr)
-    dat['LEtrans'] = (dat.s_day*dat.Ac_day+dat.rho_day*Cp*dat.vpd_day*dat.Fc*dat.daylength/dat.ra)*(1.-dat.Fwet_day)/(dat.s_day+dat.gama_day*(1+dat.rs/dat.ra))
+    dat['rs'] = 1.0 / dat.Cc
+    dat['rh'] = 1.0 / gl_sh
+    dat['rr'] = dat.rho_day * Cp / (
+            4.0 * SBC * dat.ta_day * dat.ta_day * dat.ta_day)  # (dat.ta_day+273.15)*(dat.ta_day+273.15)*(dat.ta_day+273.15)
+    dat['ra'] = (dat.rh * dat.rr) / (dat.rh + dat.rr)
+    dat['LEtrans'] = (dat.s_day * dat.Ac_day + dat.rho_day * Cp * dat.vpd_day * dat.Fc * dat.daylength / dat.ra) * (
+            1. - dat.Fwet_day) / (dat.s_day + dat.gama_day * (1 + dat.rs / dat.ra))
 
     df['LEtrans_day'] = dat.LEtrans
     return df
@@ -92,33 +90,34 @@ def LEtrans_day(df, Cl, gl_sh, vpd_open, vpd_close, tmin_open, tmin_close, sm_op
 
 # nighttime plant transpiration
 def LEtrans_night(df, gl_sh):
-
-    df['ta'] = ((df.ta_night+273.15)/293.15)**1.75
+    df['ta'] = ((df.ta_night + 273.15) / 293.15) ** 1.75
     dat = df
 
-    dat['rcorr'] = 1.0/((101300/dat.pa)*dat.ta)
+    dat['rcorr'] = 1.0 / ((101300 / dat.pa) * dat.ta)
 
     dat['Gs1'] = 0
-    dat['Gcu'] = dat.rcorr*0.00001
+    dat['Gcu'] = dat.rcorr * 0.00001
     dat['Gs2'] = gl_sh
-    dat['Cc'] = dat.LAI*(1.0-dat.Fwet_night)*(dat.Gs2*(dat.Gs1+dat.Gcu))/(dat.Gs1+dat.Gs2+dat.Gcu)
+    dat['Cc'] = dat.LAI * (1.0 - dat.Fwet_night) * (dat.Gs2 * (dat.Gs1 + dat.Gcu)) / (dat.Gs1 + dat.Gs2 + dat.Gcu)
 
     dat['Cc'] = np.where((dat['LAI'] > 0) & ((1.0 - dat['Fwet_night']) > 0), dat['Cc'], 0)
-    dat['rs'] = 1.0/dat.Cc
-    dat['rh'] = 1.0/gl_sh
-    dat['rr'] = dat.rho_night*Cp/(4.0*SBC*(dat.ta_night+273.15)*(dat.ta_night+273.15)*(dat.ta_night+273.15))
-    dat['ra'] = (dat.rh*dat.rr)/(dat.rh+dat.rr)
-    dat['LEtrans'] = (dat.s_night*dat.Ac_night+dat.rho_night*Cp*dat.vpd_night*dat.Fc*dat.nightlength/dat.ra)*(1.-dat.Fwet_night)/(dat.s_night+dat.gama_night*(1+dat.rs/dat.ra))
+    dat['rs'] = 1.0 / dat.Cc
+    dat['rh'] = 1.0 / gl_sh
+    dat['rr'] = dat.rho_night * Cp / (
+            4.0 * SBC * (dat.ta_night + 273.15) * (dat.ta_night + 273.15) * (dat.ta_night + 273.15))
+    dat['ra'] = (dat.rh * dat.rr) / (dat.rh + dat.rr)
+    dat['LEtrans'] = (
+                             dat.s_night * dat.Ac_night + dat.rho_night * Cp * dat.vpd_night * dat.Fc * dat.nightlength / dat.ra) * (
+                             1. - dat.Fwet_night) / (dat.s_night + dat.gama_night * (1 + dat.rs / dat.ra))
 
     df['LEtrans_night'] = dat.LEtrans
     return df
 
 
 def LEtrans_day_no_scalars(df, Cl, gl_sh, vpd_open, vpd_close, tmin_open, tmin_close, sm_open, sm_close):
-
-    df['ta'] = ((df.ta_day+273.15)/293.15)**1.75
+    df['ta'] = ((df.ta_day + 273.15) / 293.15) ** 1.75
     dat = df
-    dat['rcorr'] = 1.0/((101300/dat.pa)*dat.ta)
+    dat['rcorr'] = 1.0 / ((101300 / dat.pa) * dat.ta)
 
     dat = mVPD_day(dat, vpd_close=vpd_close, vpd_open=vpd_open)
     dat = mTemp_day(dat, tmin_close=tmin_close, tmin_open=tmin_open)
@@ -126,17 +125,18 @@ def LEtrans_day_no_scalars(df, Cl, gl_sh, vpd_open, vpd_close, tmin_open, tmin_c
     if sm_open is not None and sm_close is not None:
         dat = mSM(dat, sm_open=sm_open, sm_close=sm_close)
 
-    dat['Gs1'] = Cl*dat.rcorr
-    dat['Gcu'] = dat.rcorr*0.00001
+    dat['Gs1'] = Cl * dat.rcorr
+    dat['Gcu'] = dat.rcorr * 0.00001
     dat['Gs2'] = gl_sh
-    dat['Cc'] = dat.LAI*(1.0-dat.Fwet_day)*(dat.Gs2*(dat.Gs1+dat.Gcu))/(dat.Gs1+dat.Gs2+dat.Gcu)
+    dat['Cc'] = dat.LAI * (1.0 - dat.Fwet_day) * (dat.Gs2 * (dat.Gs1 + dat.Gcu)) / (dat.Gs1 + dat.Gs2 + dat.Gcu)
 
     dat['Cc'] = np.where((dat['LAI'] > 0) & ((1.0 - dat['Fwet_day']) > 0), dat['Cc'], 0)
-    dat['rs'] = 1.0/dat.Cc
-    dat['rh'] = 1.0/gl_sh
-    dat['rr'] = dat.rho_day*Cp/(4.0*SBC*(dat.ta_day+273.15)*(dat.ta_day+273.15)*(dat.ta_day+273.15))
-    dat['ra'] = (dat.rh*dat.rr)/(dat.rh+dat.rr)
-    dat['LEtrans'] = (dat.s_day*dat.Ac_day+dat.rho_day*Cp*dat.vpd_day*dat.Fc*dat.daylength/dat.ra)*(1.-dat.Fwet_day)/(dat.s_day+dat.gama_day*(1+dat.rs/dat.ra))
+    dat['rs'] = 1.0 / dat.Cc
+    dat['rh'] = 1.0 / gl_sh
+    dat['rr'] = dat.rho_day * Cp / (4.0 * SBC * (dat.ta_day + 273.15) * (dat.ta_day + 273.15) * (dat.ta_day + 273.15))
+    dat['ra'] = (dat.rh * dat.rr) / (dat.rh + dat.rr)
+    dat['LEtrans'] = (dat.s_day * dat.Ac_day + dat.rho_day * Cp * dat.vpd_day * dat.Fc * dat.daylength / dat.ra) * (
+            1. - dat.Fwet_day) / (dat.s_day + dat.gama_day * (1 + dat.rs / dat.ra))
 
     df['LEtrans_day_no_scalars'] = dat.LEtrans
     return df
@@ -144,7 +144,7 @@ def LEtrans_day_no_scalars(df, Cl, gl_sh, vpd_open, vpd_close, tmin_open, tmin_c
 
 # potential plant transpiration is calculated following the Priestley-Taylor
 def PLE_plant_day(df):
-    df['PLE_plant_day'] = (1.26*df.s_day*df.Ac_day*(1-df.Fwet_day)/(df.s_day+df.gama_day))
+    df['PLE_plant_day'] = (1.26 * df.s_day * df.Ac_day * (1 - df.Fwet_day) / (df.s_day + df.gama_day))
     return df
 
 
@@ -155,7 +155,6 @@ def PLE_plant_night(df):
 
 # ------------------------------------------Soil surface evaporation calculations--------------
 def rtot_day(df, rbl_max, rbl_min, vpd_close, vpd_open):
-
     rblmax = rbl_max
     rblmin = rbl_min
     vpdclose = vpd_close
@@ -164,18 +163,17 @@ def rtot_day(df, rbl_max, rbl_min, vpd_close, vpd_open):
     df['ta'] = ((df.ta_day + 273.15) / 293.15) ** 1.75
     dat = df
     dat['rcorr'] = 1.0 / ((101300 / dat.pa) * dat.ta)
-    dat['rtotc'] = rblmax-((rblmax-rblmin)*(vpdclose-dat.vpd_day))/(vpdclose-vpdopen)
+    dat['rtotc'] = rblmax - ((rblmax - rblmin) * (vpdclose - dat.vpd_day)) / (vpdclose - vpdopen)
 
     dat['rtotc'] = np.where((dat['vpd_day'] <= vpdopen), rblmax, dat['rtotc'])
     dat['rtotc'] = np.where((dat['vpd_day'] >= vpdclose), rblmin, dat['rtotc'])
-    dat['rtot_day'] = dat.rtotc*dat.rcorr
+    dat['rtot_day'] = dat.rtotc * dat.rcorr
     df['rtot_day'] = dat.rtot_day
 
     return df
 
 
 def rtot_night(df, rbl_max, rbl_min, vpd_close, vpd_open):
-
     rblmax = rbl_max
     rblmin = rbl_min
     vpdclose = vpd_close
@@ -184,78 +182,78 @@ def rtot_night(df, rbl_max, rbl_min, vpd_close, vpd_open):
     df['ta'] = ((df.ta_night + 273.15) / 293.15) ** 1.75
     dat = df
     dat['rcorr'] = 1.0 / ((101300 / dat.pa) * dat.ta)
-    dat['rtotc'] = rblmax-((rblmax-rblmin)*(vpdclose-dat.vpd_night))/(vpdclose-vpdopen)
+    dat['rtotc'] = rblmax - ((rblmax - rblmin) * (vpdclose - dat.vpd_night)) / (vpdclose - vpdopen)
 
     dat['rtotc'] = np.where((dat['vpd_night'] <= vpdopen), rblmax, dat['rtotc'])
     dat['rtotc'] = np.where((dat['vpd_night'] >= vpdclose), rblmin, dat['rtotc'])
-    dat['rtot_night'] = dat.rtotc*dat.rcorr
+    dat['rtot_night'] = dat.rtotc * dat.rcorr
     df['rtot_night'] = dat.rtot_night
 
     return df
 
 
 def LEwetsoil_day(df):
-
-    df['ras'] = df.rtot_day*df.rrs_day/(df.rtot_day+df.rrs_day)
-    df['LEwetsoil_day'] = (df.s_day*df.Asoil_day+df.rho_day*Cp*(1.0-df.Fc)*df.vpd_day*df.daylength/df.ras)*df.Fwet_day / (df.s_day+df.gama_day*df.rtot_day/df.ras)
+    df['ras'] = df.rtot_day * df.rrs_day / (df.rtot_day + df.rrs_day)
+    df['LEwetsoil_day'] = (df.s_day * df.Asoil_day + df.rho_day * Cp * (
+            1.0 - df.Fc) * df.vpd_day * df.daylength / df.ras) * df.Fwet_day / (
+                                  df.s_day + df.gama_day * df.rtot_day / df.ras)
 
     return df
 
 
 def LEwetsoil_night(df):
-
-    df['ras'] = df.rtot_night*df.rrs_night/(df.rtot_night+df.rrs_night)
-    df['LEwetsoil_night'] = (df.s_night*df.Asoil_night+df.rho_night*Cp*(1.0-df.Fc)*df.vpd_night*df.nightlength/df.ras)*df.Fwet_night / (df.s_night+df.gama_night*df.rtot_night/df.ras)
+    df['ras'] = df.rtot_night * df.rrs_night / (df.rtot_night + df.rrs_night)
+    df['LEwetsoil_night'] = (df.s_night * df.Asoil_night + df.rho_night * Cp * (
+            1.0 - df.Fc) * df.vpd_night * df.nightlength / df.ras) * df.Fwet_night / (
+                                    df.s_night + df.gama_night * df.rtot_night / df.ras)
 
     return df
 
 
 # potential soil evaporation
 def PLE_soil_night(df):
-
-    df['ras']=df.rtot_night * df.rrs_night / (df.rtot_night + df.rrs_night)
-    df['PLEsoil_night'] = (df.s_night*df.Asoil_night+df.rho_night*Cp*(1.0-df.Fc)*df.vpd_night*df.nightlength/df.ras) * \
-                          (1.0-df.Fwet_night)/(df.s_night+df.gama_night*df.rtot_night/df.ras)
+    df['ras'] = df.rtot_night * df.rrs_night / (df.rtot_night + df.rrs_night)
+    df['PLEsoil_night'] = (df.s_night * df.Asoil_night + df.rho_night * Cp * (
+            1.0 - df.Fc) * df.vpd_night * df.nightlength / df.ras) * \
+                          (1.0 - df.Fwet_night) / (df.s_night + df.gama_night * df.rtot_night / df.ras)
 
     return df
 
 
 def PLE_soil_day(df):
-
     df['ras'] = df.rtot_day * df.rrs_day / (df.rtot_day + df.rrs_day)
-    df['PLEsoil_day'] = (df.s_day*df.Asoil_day+df.rho_day*Cp*(1.0-df.Fc)*df.vpd_day*df.daylength/df.ras) * \
-                        (1.0-df.Fwet_day)/(df.s_day+df.gama_day*df.rtot_day/df.ras)
+    df['PLEsoil_day'] = (df.s_day * df.Asoil_day + df.rho_day * Cp * (
+            1.0 - df.Fc) * df.vpd_day * df.daylength / df.ras) * \
+                        (1.0 - df.Fwet_day) / (df.s_day + df.gama_day * df.rtot_day / df.ras)
 
     return df
 
 
 # total soil LE
 def LEsoil_day(df, beta, sm_open, sm_close):
-
     df = PLE_soil_day(df)
     df = LEwetsoil_day(df)
 
     if sm_open is not None and sm_close is not None:
         df['fSM_day'] = df.fSM
     else:
-        df['fSM_day'] = (df.rh*0.01) ** (df.vpd_day/beta)
+        df['fSM_day'] = (df.rh * 0.01) ** (df.vpd_day / beta)
 
-    df['LEsoil_day'] = (df.PLEsoil_day*df.fSM_day)+df.LEwetsoil_day
+    df['LEsoil_day'] = (df.PLEsoil_day * df.fSM_day) + df.LEwetsoil_day
     return df
 
 
 # total soil LE
 def LEsoil_night(df, beta, sm_open, sm_close):
-
     df = PLE_soil_night(df)
     df = LEwetsoil_night(df)
 
     if sm_open is not None and sm_close is not None:
         df['fSM_night'] = df.fSM
     else:
-        df['fSM_night'] = (df.rh*0.01) ** (df.vpd_night/beta)
+        df['fSM_night'] = (df.rh * 0.01) ** (df.vpd_night / beta)
 
-    df['LEsoil_night'] = (df.PLEsoil_night*df.fSM_night)+df.LEwetsoil_night
+    df['LEsoil_night'] = (df.PLEsoil_night * df.fSM_night) + df.LEwetsoil_night
     return df
 
 
@@ -263,7 +261,6 @@ def LEsoil_night(df, beta, sm_open, sm_close):
 # calculate total ET
 def MOD16(df, tmin_open, tmin_close, vpd_close, vpd_open, gl_sh,
           gl_e_wv, Cl, rbl_min, rbl_max, beta=None, sm_open=None, sm_close=None, no_scalars=False):
-
     assert beta or sm_open, 'Either beta or sm_close and sm_open must be included as parameters.'
     assert (beta and not sm_open) or (sm_open and not beta), 'Cannot run the model with both beta and sm parameters.'
 
@@ -293,8 +290,8 @@ def MOD16(df, tmin_open, tmin_close, vpd_close, vpd_open, gl_sh,
     df['LE_day'] = df.LEwet_day + df.LEtrans_day + df.LEsoil_day
     df['LE_night'] = df.LEwet_night + df.LEtrans_night + df.LEsoil_night
 
-    df['ET_day'] = df.LE_day/df.lamda_day
-    df['ET_night'] = df.LE_night/df.lamda_night
+    df['ET_day'] = df.LE_day / df.lamda_day
+    df['ET_night'] = df.LE_night / df.lamda_night
     df['LE'] = df.LE_day + df.LE_night
     df['simulation'] = df.ET_day + df.ET_night
 